395bfe539ec8b6d92f79b093977c8d31375c3a88
KaveeshBaddage <kbaddage98@gmail.com>
2020-11-11 12:32:45 +0530

    Set setHandlerProperty in Validate method

--- mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/smsmessaging/SendSMSHandler.java
+++ mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/smsmessaging/SendSMSHandler.java
@@ -290,6 +290,7 @@ public class SendSMSHandler extends AbstractHandler{
 	 */
 	@Override
 	public boolean validate(String httpMethod, String requestPath, JSONObject jsonBody, MessageContext context) throws Exception {
+		HandlerUtils.setHandlerProperty(context, this.getClass().getSimpleName());
 		if (!httpMethod.equalsIgnoreCase("POST")) {
 			((Axis2MessageContext) context).getAxis2MessageContext().setProperty("HTTP_SC", 405);
 			throw new Exception("Method not allowed");
